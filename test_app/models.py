2a2b6bd941b3ec272d14adfd4a1cbea9b41d2e0a
borebrian2021 <bkimutai2021@gmail.com>
2023-04-19 12:11:16 +0300

    Added the admin managemet module

--- test_app/models.py
+++ test_app/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-
-# Create your models here.
-# class TestModel(models.Model):
-    # models.CharField()
-    # models.TextField()
-    # models.BooleanField()
-    # models.oneToManyField()
-    # models.ForeignKey()
+from django.utils import timezone
 class TestModel(models.Model):
     name=models.CharField(max_length=255,unique=True,null=True,blank=True)
     description =models.TextField()
     phone_number=models.PositiveBigIntegerField()
-    is_live=models.BooleanField()
+    is_alive=models.BooleanField()
     amount=models.FloatField()
+    created_at =models.DateTimeField(auto_now_add=True)
+    updated_at =models.DateTimeField(auto_now=True)
         
+
+    def __str__(self):
+        return f"{self.name} - {self.created_at.strftime('%H: %M: %S')} "
     
+    class Meta:
+        ordering = ("-amount",)
