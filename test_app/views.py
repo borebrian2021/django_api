bd8149c4de5ad801f3483dee55f226b50f99d5d8
borebrian2021 <bkimutai2021@gmail.com>
2023-04-20 07:39:51 +0300

    begining endpoints intergration

--- test_app/views.py
+++ test_app/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse, response
 
-# Create your views here.
+def simple(request):
+    
+    return response.HttpResponse("Welcome home")
