0d8060f937986242040a65d989ada61bf68bb62a
borebrian2021 <bkimutai2021@gmail.com>
2023-04-20 08:28:20 +0300

    Added the requests

--- django_api/urls.py
+++ django_api/urls.py
@@ -1,7 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
+from test_app.views import simple
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('simple/',simple)
+    
 ]
--- test_app/views.py
+++ test_app/views.py
@@ -1,5 +1,14 @@
 from django.http import JsonResponse, response
+from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
 def simple(request):
-    
-    return response.HttpResponse("Welcome home")
+    method = request.method.lower()
+    if method == 'get':
+        return JsonResponse({"data":[1,2,3,4,5]})
+    elif method == 'post':
+        return JsonResponse({"data":"Added successfully"})
+    elif method == 'put':
+        return JsonResponse({"data":"Updated successfully"})
+    return JsonResponse({"error":"Method not allowed"})
