cd972608cfe9f7dcbe27757001aef43f139e8a54
borebrian2021 <bkimutai2021@gmail.com>
2023-04-22 20:24:47 +0300

    Added update functionality

--- test_app/serializers.py
+++ test_app/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from .models import TestModel
 
 class SimpleSerializer(serializers.Serializer):
     name=serializers.CharField()
@@ -6,8 +7,16 @@ class SimpleSerializer(serializers.Serializer):
     phone_number=serializers.IntegerField()
     is_alive=serializers.BooleanField()
     amount=serializers.FloatField()
-    extra_name=serializers.CharField()
-    created_at =serializers.DateTimeField()
-    updated_at =serializers.DateTimeField()
+    extra_name=serializers.CharField(read_only=True)
+    created_at =serializers.DateTimeField(read_only=True)
+    updated_at =serializers.DateTimeField(read_only=True)
        
+       
+    def create(self,validated_data):
         
+        return TestModel.objects.create(**validated_data)
+        
+        
+    def update(self,instance,validated_data):
+        TestModel.objects.filter(id=instance.id).update(**validated_data)
+        return TestModel.objects.get(id=instance.id)
--- test_app/views.py
+++ test_app/views.py
@@ -9,21 +9,40 @@ class Simple(APIView):
         # VALIDATING THE DATA INPUT
         serializer= SimpleSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
         
-        # CREATING AND INSERTING THE NEW DATA
-        new_test_content=TestModel.objects.create(
-            name=request.data['name'],
-            description =request.data['description'],
-            phone_number = request.data['phone_number'],
-            is_alive = request.data['is_alive'],
-            amount = request.data['amount'],
-        )
+        # # CREATING AND INSERTING THE NEW DATA
+        # new_test_content=TestModel.objects.create(
+        #     name=request.data['name'],
+        #     description =request.data['description'],
+        #     phone_number = request.data['phone_number'],
+        #     is_alive = request.data['is_alive'],
+        #     amount = request.data['amount'],
+        # )
         
         #RETURNING THE DATA TO THE FRONTEND
-        return JsonResponse({"data":SimpleSerializer(new_test_content).data})
+        return JsonResponse({"data":serializer.data})
         
        
     
     def get(self, request):
         content =TestModel.objects.all().values()
         return JsonResponse({"data":SimpleSerializer(content,many=True).data})
+    
+    
+    def put(self,request ,*args, **kwargs):
+        model_id = kwargs.get("id",None)
+        if not model_id:
+            return JsonResponse({"error": "method /PUT/ not allowed"})
+        
+        try:
+            instance=TestModel.objects.get(id=model_id)
+        except:
+            return JsonResponse({"error": "Object does not exists"})
+        
+        serializer = SimpleSerializer(data=request.data,instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return JsonResponse({"data": serializer.data})
+
+                
